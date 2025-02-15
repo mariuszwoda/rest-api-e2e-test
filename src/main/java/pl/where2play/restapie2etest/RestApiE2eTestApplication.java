e985379cbb0a44e356768f4129409d4081eb800e
mariusz woda <mariusz.woda@gmail.com>
2025-02-15 11:47:28 +0100

    first commit

---
+++ src/main/java/pl/where2play/restapie2etest/RestApiE2eTestApplication.java
@@ -0,0 +1,13 @@
+package pl.where2play.restapie2etest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiE2eTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiE2eTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/where2play/restapie2etest/RestApiE2eTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.where2play.restapie2etest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiE2eTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
